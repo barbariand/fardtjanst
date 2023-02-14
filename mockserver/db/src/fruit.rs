ddf2fdcb349cb36d9d859b5e5d4a2c4d794cbc60
barbariand <dante@nilsson.name>
2023-02-14 13:41:17 +0100

    as previus failed to commit rn

---
+++ mockserver/db/src/fruit.rs
@@ -0,0 +1,33 @@
+use sea_orm;
+use sea_orm::entity::prelude::*;
+
+#[cfg(feature = "with-json")]
+use serde::{Deserialize, Serialize};
+#[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
+#[cfg_attr(feature = "with-json", derive(Serialize, Deserialize))]
+#[sea_orm(table_name = "fruit")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    #[cfg_attr(feature = "with-json", serde(skip_deserializing))]
+    pub id: i32,
+    pub name: String,
+    pub cake_id: Option<i32>,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::cake::Entity",
+        from = "Column::CakeId",
+        to = "super::cake::Column::Id"
+    )]
+    Cake,
+}
+
+impl Related<super::cake::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Cake.def()
+    }
+}
+
+impl ActiveModelBehavior for ActiveModel {}
--- mockserver/src/main.rs
+++ mockserver/src/main.rs
@@ -1,10 +1,21 @@
 use actix_rt;
 use actix_web::{middleware::Logger, web, App, HttpResponse, HttpServer};
-use db;
+use db::{cake, fruit, getdb, sea_orm::*, Cake};
 use env_logger::Env;
+use futures::executor::block_on;
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    db::add(1, 2);
+    let db = match block_on(getdb()) {
+        Ok(db) => db,
+        Err(err) => panic!("Could not create database: {}", err),
+    };
+
+    let cheese = cake::ActiveModel {
+        name: ActiveValue::Set("Happy Bakery".to_owned()),
+        id: ActiveValue::Set(1),
+    };
+    let dada = Cake::insert(cheese).exec(&db).await.expect("msg");
+    
     env_logger::init_from_env(
         Env::default()
             .default_filter_or("info")
