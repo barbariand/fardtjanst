9997059fd33a2c04df1ccce578f910451563f6d7
barbariand <dante@nilsson.name>
2023-02-17 15:23:50 +0100

    Created correct db and imported sessions

--- mockserver/src/main.rs
+++ mockserver/src/main.rs
@@ -1,28 +1,53 @@
 use actix_rt;
-use actix_web::{middleware::Logger, web, App, HttpResponse, HttpServer};
-use db::{cake, fruit, getdb, sea_orm::*, Cake};
+use actix_session::*;
+use actix_web::{get, middleware::Logger, web, App, HttpResponse, HttpServer, Result};
+use db::{getdb, resor, sea_orm::*, users, Resor, Users};
 use env_logger::Env;
+
 use futures::executor::block_on;
+struct App_data {
+    db: DatabaseConnection,
+}
+impl Clone for App_data {
+    fn clone(&self) -> Self {
+        Self {
+            db: self.db.clone(),
+        }
+    }
+}
+impl App_data {
+    fn get_db(&self) -> &db::DatabaseConnection {
+        &self.db
+    }
+    fn new(db: db::DatabaseConnection) -> App_data {
+        App_data { db }
+    }
+    fn to_String(&self) -> String {
+        "Hello".to_string()
+    }
+}
+#[get("/autherization")]
+async fn autherization(data: web::Data<App_data>) -> Result<String> {
+    Ok(data.to_String())
+}
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let db = match block_on(getdb()) {
-        Ok(db) => db,
-        Err(err) => panic!("Could not create database: {}", err),
-    };
-
-    let cheese = cake::ActiveModel {
-        name: ActiveValue::Set("Happy Bakery".to_owned()),
-        id: ActiveValue::Set(1),
-    };
-    let dada = Cake::insert(cheese).exec(&db).await.expect("msg");
-    
+    //gets database and
+    let db = block_on(getdb()).expect("Could not create or find database:");
     env_logger::init_from_env(
         Env::default()
             .default_filter_or("info")
             .default_filter_or("debug"),
     );
-    HttpServer::new(move || App::new().wrap(Logger::default()).wrap(Logger::new(" t")))
-        .bind("127.0.0.1:5376")?
-        .run()
-        .await
+    let app_data = App_data::new(db);
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(app_data.clone()))
+            .wrap(Logger::default())
+            .service(autherization)
+    })
+    .bind("127.0.0.1:5376")?
+    .run()
+    .await
 }
