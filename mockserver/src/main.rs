981e8a7fde36672f634d4bb576f254c346315daf
barbariand <dante@nilsson.name>
2023-02-18 15:16:02 +0100

    started implementing sessions

--- mockserver/src/main.rs
+++ mockserver/src/main.rs
@@ -1,38 +1,44 @@
 use actix_rt;
-use actix_session::*;
-use actix_web::{get, middleware::Logger, web, App, HttpResponse, HttpServer, Result};
+use actix_session::config::SessionLifecycle;
+use actix_session::{storage::CookieSessionStore, Session, SessionMiddleware};
+use actix_web::cookie::Key;
+use actix_web::{get, middleware::Logger, post, web, App, HttpResponse, HttpServer, Result};
 use db::{getdb, resor, sea_orm::*, users, Resor, Users};
 use env_logger::Env;
-
 use futures::executor::block_on;
-struct App_data {
+struct AppData {
     db: DatabaseConnection,
 }
-impl Clone for App_data {
+impl Clone for AppData {
     fn clone(&self) -> Self {
         Self {
             db: self.db.clone(),
         }
     }
 }
-impl App_data {
+impl AppData {
     fn get_db(&self) -> &db::DatabaseConnection {
         &self.db
     }
-    fn new(db: db::DatabaseConnection) -> App_data {
-        App_data { db }
+    fn new(db: db::DatabaseConnection) -> AppData {
+        AppData { db }
     }
-    fn to_String(&self) -> String {
+    fn to_string(&self) -> String {
         "Hello".to_string()
     }
 }
 #[get("/autherization")]
-async fn autherization(data: web::Data<App_data>) -> Result<String> {
-    Ok(data.to_String())
+async fn autherization(data: web::Data<AppData>, session: Session) -> Result<String> {
+    session
+        .insert("test", "testvalue")
+        .expect("failed json serilisation");
+
+    Ok(data.to_string())
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    let secret_key = Key::generate();
     //gets database and
     let db = block_on(getdb()).expect("Could not create or find database:");
     env_logger::init_from_env(
@@ -40,11 +46,15 @@ async fn main() -> std::io::Result<()> {
             .default_filter_or("info")
             .default_filter_or("debug"),
     );
-    let app_data = App_data::new(db);
+    let app_data = AppData::new(db);
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(app_data.clone()))
             .wrap(Logger::default())
+            .wrap(
+                SessionMiddleware::builder(CookieSessionStore::default(), secret_key.clone())
+                    .session_lifecycle(SessionLifecycle),
+            )
             .service(autherization)
     })
     .bind("127.0.0.1:5376")?
