d23f8fd5d6a4efc04bee7939d4b489a186a4d17c
barbariand <dante@nilsson.name>
2023-05-20 15:31:36 +0200

    merged dbs to a mod instead of a lib and started with notifications

---
+++ server/src/db/tables/notification_info.rs
@@ -0,0 +1,37 @@
+use sea_orm;
+use sea_orm::entity::prelude::*;
+
+
+#[cfg(feature = "with-json")]
+use serde::{Deserialize, Serialize};
+#[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
+#[cfg_attr(feature = "with-json", derive(Serialize, Deserialize))]
+#[sea_orm(table_name = "notificationinfo")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    #[cfg_attr(feature = "with-json", serde(skip_deserializing))]
+    pub id: i32,
+    pub UserId:i32,
+    pub endpoint:String,
+    pub p256dh: String,
+    pub auth: String,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::users::Entity",
+        from = "Column::UserId",
+        to = "super::users::Column::Id"
+    )]
+    Users,
+}
+impl Related<super::users::Entity> for Entity {
+    fn to() -> RelationDef {
+        
+        Relation::Users.def()
+
+    }
+}
+
+impl ActiveModelBehavior for ActiveModel {}
