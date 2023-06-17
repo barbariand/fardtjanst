04d676162f033d36c29b5bf393e38553f68e61ef
barbariand <dante@nilsson.name>
2023-06-17 14:38:09 +0200

    fixing stuf

--- server/src/routes/api/mod.rs
+++ server/src/routes/api/mod.rs
@@ -1,1 +1,60 @@
-mod autherization;
+mod autherization_route;
+mod order_route;
+mod registernewuser;
+use std::ops::Deref;
+
+use crate::MOCK_SERVER_URL;
+use actix_web::http::header;
+pub use autherization_route::autherization;
+use futures_util::Future;
+pub use registernewuser::register_user;
+use reqwest::{Client, IntoUrl, RequestBuilder, Response};
+use serde::{Deserialize, Serialize};
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct User {
+    pub username: i32,
+    pub password: String,
+}
+
+pub enum RequestError {
+    Reqwest(reqwest::Error),
+    SerdeJson(serde_json::Error),
+    Header,
+}
+impl From<reqwest::Error> for RequestError {
+    fn from(value: reqwest::Error) -> Self {
+        RequestError::Reqwest(value)
+    }
+}
+impl From<serde_json::Error> for RequestError {
+    fn from(value: serde_json::Error) -> Self {
+        RequestError::SerdeJson(value)
+    }
+}
+pub async fn execute_request_for<U>(
+    user: User,
+    method: reqwest::Method,
+    url: U,
+) -> Result<RequestBuilder, RequestError>
+where
+    U: IntoUrl,
+{
+    let client = reqwest::Client::new();
+    let cookie = get_correct_headers(user).await?;
+    Ok(client.request(method, url).header("Cookie", cookie))
+}
+pub async fn get_correct_headers(
+    user: User,
+) -> Result<actix_http::header::HeaderValue, RequestError> {
+    let client = reqwest::Client::new();
+    let auth_response = client
+        .post(MOCK_SERVER_URL.to_string() + "/api/autherization")
+        .body(serde_json::to_string(&user)?)
+        .send()
+        .await?;
+    Ok(auth_response
+        .headers()
+        .get("id")
+        .ok_or_else(|| RequestError::Header)?
+        .clone())
+}
--- server/src/routes/api/registernewuser.rs
+++ server/src/routes/api/registernewuser.rs
@@ -1,6 +1,18 @@
-use actix::post;
-#[post("/api/autherization")]
-async fn autherization(
+use crate::{AppData, MAX_PAYLOAD_SIZE, MOCK_SERVER_URL};
+use actix_session::Session;
+use actix_web::{error, post, web};
+use actix_web::{HttpResponse, Responder,HttpRequest};
+use actix_web::Result;
+use std::fmt::Display;
+use crate::db;
+use db::{
+    sea_orm::{ColumnTrait, EntityTrait, QueryFilter},
+    tempsessions, users, TempSession, Users,
+};
+use futures::StreamExt;
+use log::error;
+use super::{User,RequestError};
+async fn register_user(
     data: web::Data<AppData>,
     session: Session,
     mut payload: web::Payload,
@@ -13,5 +25,26 @@ async fn autherization(
         }
         body.extend_from_slice(&chunk);
     }
-
+    let user = match serde_json::from_slice::<User>(&body) {
+        Ok(au) => au,
+        Err(e) => {
+            error!(target:"/api/autherization",
+                "faild to serilze {:?} into {:?}, got error {}",
+                body,
+                User {
+                    ..Default::default()
+                },
+                e
+            );
+            return Err(error::ErrorBadRequest("Invalid json"));
+        }
+    };
+    if let Err(e)=super::execute_request_for(user, reqwest::Method::POST, MOCK_SERVER_URL.to_string()+"/").await{
+        match e{
+            RequestError::Reqwest(_) => todo!(),
+            RequestError::SerdeJson(_) => Ok(HttpResponse::ServiceUnavailable()),
+            RequestError::Header => Ok(HttpResponse::BadGateway()),
+        }
+    }
+    Ok("not implimented")
 }
